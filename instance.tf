10249c4f5274a5567c4ff2913962b59a998aa981
nick-otter <nicholas.a.otter@gmail.com>
2017-11-23 17:48:17 +0000

    moving provider

--- instance.tf
+++ instance.tf
@@ -1,7 +1,3 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
-
 variable "tableau_ami_id" {}
 variable "tableau_instance_type" {}
 variable "tableau_user_data" {}
@@ -13,12 +9,6 @@ variable "security_cidr" {}
 
 variable "vpc_id" {}
 variable "subnet_cidr" {}
-
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-}
 
 resource "aws_instance" "internal_tableau" {
   ami           = "${var.tableau_ami_id}"
@@ -49,6 +39,8 @@ resource "aws_security_group" "internal_tableau" {
 resource "aws_subnet" "internal_tableau" {
   vpc_id     = "${var.vpc_id}"
   cidr_block = "${var.subnet_cidr}"
+
+
 
   tags {
     Name = "internal_tableau"
