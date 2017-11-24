9d0d88afb48f7482922c98070e1afda1e3886aa5
nick-otter <nicholas.a.otter@gmail.com>
2017-11-24 11:27:43 +0000

    added ingress and egress

--- instance.tf
+++ instance.tf
@@ -6,6 +6,8 @@ variable "subnet_id" {}
 variable "protocol" {}
 variable "security_cidr" {}
 
+variable "postgres_security_groups" {}
+
 
 resource "aws_instance" "internal_tableau" {
   ami           = "${var.tableau_ami_id}"
@@ -30,6 +32,21 @@ resource "aws_security_group" "internal_tableau" {
     from_port = 443
     to_port = 443
     protocol = "${var.protocol}"
+    cidr_blocks = "${var.security_cidr}"
+  }
+
+  # gp
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = ["${var.security_groups}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
     cidr_blocks = "${var.security_cidr}"
   }
 }
