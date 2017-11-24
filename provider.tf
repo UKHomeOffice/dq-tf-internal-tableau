083fc1114fd5d1b6d9beb11bc487fdd5866297fe
nick-otter <nicholas.a.otter@gmail.com>
2017-11-24 16:36:25 +0000

    cleaning up

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  access_key = "${var.TF_VAR_aws_access_key}"
-  secret_key = "${var.TF_VAR_aws_access_key}"
-  region = "${var.region}"
-}
-
--- vars.tf
+++
@@ -1,4 +0,0 @@
-variable "TF_VAR_aws_access_key" {}
-variable "TF_VAR_aws_access_key" {}
-variable "region" {}
-
