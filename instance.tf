d1da2ebf4df96831ea5003349bc69d45cc3355cd
nick-otter <nicholas.a.otter@gmail.com>
2017-11-24 16:36:25 +0000

    cleaning up

--- instance.tf
+++ instance.tf
@@ -2,26 +2,23 @@ variable "tableau_ami_id" {}
 variable "tableau_instance_type" {}
 variable "tableau_user_data" {}
 variable "subnet_id" {}
+variable "GREENPLUM_IP" {}
 
 variable "protocol" {}
-variable "security_cidr" {}
+variable "ingress_cidr" {}
+variable "egress_cidr" {}
 
 variable "postgres_security_groups" {}
-
 
 resource "aws_instance" "internal_tableau" {
   ami           = "${var.tableau_ami_id}"
   instance_type = "${var.tableau_instance_type}"
-  subnet_id = "${var.subnet_id}"
+  subnet_id     = "${var.subnet_id}"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+  user_data = "LISTEN_HTTP=0.0.0.0:443 CHECK_GP=${var.GREENPLUM_IP}:5432"
 
   tags {
-    Name = "internal-tableau"
+    Name = "internal-tableau" # see naming scheme
   }
 }
 
@@ -29,26 +26,15 @@ resource "aws_security_group" "internal_tableau" {
   name = "internal_tableau"
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "${var.protocol}"
-    cidr_blocks = "${var.security_cidr}"
-  }
-
-  # gp
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = ["${var.security_groups}"]
-  }
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = "${var.ingress_cidr}"
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = -1
-    cidr_blocks = "${var.security_cidr}"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = "${var.egress_cidr}"
   }
 }
-
-
