d44185383aaff37269aac34626aa2d155a22fcff
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-05 20:35:04 +0000

    use connectivity tester

--- main.tf
+++ main.tf
@@ -1,32 +1,15 @@
-data "aws_ami" "linux_connectivity_tester" {
-  most_recent = true
+module "instance" {
+  source    = "github.com/UKHomeOffice/connectivity-tester-tf"
+  subnet_id = "${aws_subnet.subnet.id}"
+  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_HTTP=0.0.0.0:443 LISTEN_HTTP=0.0.0.0:3389 CHECK_GP=${var.greenplum_ip}:5432"
 
-  filter {
-    name = "name"
-
-    values = [
-      "connectivity-tester-linux*",
-    ]
-  }
-
-  owners = [
-    "093401982388",
-  ]
-}
-
-resource "aws_instance" "instance" {
-  instance_type          = "${var.instance_type}"
-  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
-  subnet_id              = "${aws_subnet.subnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.sgrp.id}"]
-  user_data              = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_HTTP=0.0.0.0:443 LISTEN_HTTP=0.0.0.0:3389 CHECK_GP=${var.greenplum_ip}:5432"
-
-  tags {
-    Name             = "instance-tableau-internal-{1}-${var.service}-${var.environment}"
-    Service          = "${var.service}"
-    Environment      = "${var.environment}"
-    EnvironmentGroup = "${var.environment_group}"
-  }
+  //
+  //  tags {
+  //    Name             = "instance-tableau-internal-{1}-${var.service}-${var.environment}"
+  //    Service          = "${var.service}"
+  //    Environment      = "${var.environment}"
+  //    EnvironmentGroup = "${var.environment_group}"
+  //  }
 }
 
 resource "aws_subnet" "subnet" {
