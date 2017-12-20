a398a594c96e01fd5c13180c625ec7089e5f0214
david-wirelab <info@wirelab.co.uk>
2017-12-20 16:09:34 +0000

    add peering cidr

--- main.tf
+++ main.tf
@@ -39,16 +39,22 @@ resource "aws_security_group" "sgrp" {
     to_port   = "${var.https_to_port}"
     protocol  = "${var.https_protocol}"
 
-    cidr_blocks = ["${var.dq_ops_ingress_cidr}",
+    cidr_blocks = [
+      "${var.dq_ops_ingress_cidr}",
       "${var.acp_prod_ingress_cidr}",
+      "${var.peering_cidr_block}",
     ]
   }
 
   ingress {
-    from_port   = "${var.RDP_from_port}"
-    to_port     = "${var.RDP_to_port}"
-    protocol    = "${var.RDP_protocol}"
-    cidr_blocks = ["${var.dq_ops_ingress_cidr}"]
+    from_port = "${var.RDP_from_port}"
+    to_port   = "${var.RDP_to_port}"
+    protocol  = "${var.RDP_protocol}"
+
+    cidr_blocks = [
+      "${var.dq_ops_ingress_cidr}",
+      "${var.peering_cidr_block}",
+    ]
   }
 
   egress {
--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -22,6 +22,7 @@ class TestE2E(unittest.TestCase):
               acp_prod_ingress_cidr        = "10.5.0.0/16"
               dq_ops_ingress_cidr          = "10.2.0.0/16"
               dq_internal_dashboard_subnet_cidr                 = "10.1.12.0/24"
+              peering_cidr_block           = "1.1.1.0/24"
               greenplum_ip                 = "foo"
               apps_vpc_id                  = "foo"
             }
