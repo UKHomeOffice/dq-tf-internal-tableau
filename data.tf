c100dfe828d7567083a5f01c82c0a9c52d3008bc
david-wirelab <info@wirelab.co.uk>
2018-09-07 11:04:55 +0100

    add new EC2 instance for green/blue

--- data.tf
+++ data.tf
@@ -33,3 +33,19 @@ data "aws_ami" "int_tableau_2018_02" {
     "self",
   ]
 }
+
+data "aws_ami" "int_tableau_magic_machine" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "dq-tab-int-magic-machine",
+    ]
+  }
+
+  owners = [
+    "self",
+  ]
+}
