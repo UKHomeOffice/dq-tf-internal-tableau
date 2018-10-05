e47a89b7d3f70d366907b6785f1a68a062d515f7
Duncan Mountford <duncan9562@gmail.com>
2018-10-05 18:49:20 +0100

    Remove tableau_magic_machine

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -77,9 +77,6 @@ class TestE2E(unittest.TestCase):
     def test_ec2_secondary_tags(self):
         self.assertEqual(self.result["root_modules"]["aws_instance.int_tableau_2018_02"]["tags.Name"], "ec2-internal-tableau-2018-02-apps-preprod-dq")
 
-    def test_ec2_magic_machine_tags(self):
-        self.assertEqual(self.result["root_modules"]["aws_instance.int_tableau_magic_machine"]["tags.Name"], "ec2-internal-tableau-magic-machine-apps-preprod-dq")        
-
 
 if __name__ == '__main__':
     unittest.main()
