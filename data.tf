f2d7df6efa5ed008701a5567ec1ec286b27a4c4c
Ian Relf <ian.relf@gmail.com>
2019-01-18 16:26:55 +0000

    Still trying to get data.tf to work

--- data.tf
+++ data.tf
@@ -1,3 +1,23 @@
+data "aws_region" "current" {
+  current = true
+}
+
+data "aws_ami" "int_tableau" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "dq-tableau-17*",
+    ]
+  }
+
+  owners = [
+    "self",
+  ]
+}
+
 data "aws_ami" "int_tableau_linux" {
   most_recent = true
 
@@ -5,7 +25,7 @@ data "aws_ami" "int_tableau_linux" {
     name = "name"
 
     values = [
-      "dq-tableau-linux-20 *",
+      "dq-tableau-linux-20*",
     ]
   }
 
