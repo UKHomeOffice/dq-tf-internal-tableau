20a817b474f38fc5835865a7e887f3995b2115c9
Ian Relf <ian.relf@gmail.com>
2019-01-21 12:39:34 +0000

    Add permission to access ParamStore vars. Add logging. Remove PEERING.

--- iam.tf
+++ iam.tf
@@ -33,7 +33,10 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/int_tableau_hostname",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_server_username",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_server_password",
-        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_s3_prefix"
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_s3_prefix",
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_linux_ssh_private_key",
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_linux_ssh_public_key",
+        "arn:aws:ssm:eu-west-2:*:parameter/DATA_ARCHIVE_TAB_INT_BACKUP_URL"
       ]
     }
   ]
