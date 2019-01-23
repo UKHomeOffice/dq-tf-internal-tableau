47072e6a29b48ff386a9b2b2432dcd6c12d1f5cd
Ian Relf <ian.relf@gmail.com>
2019-01-23 14:21:03 +0000

    Removed duplicate lines.

--- iam.tf
+++ iam.tf
@@ -40,8 +40,6 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_url",
         "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_host",
         "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_port",
-        "arn:aws:ssm:eu-west-2:*:parameter/tableau_server_username",
-        "arn:aws:ssm:eu-west-2:*:parameter/tableau_server_password",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_admin_username",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_admin_password"
       ]
