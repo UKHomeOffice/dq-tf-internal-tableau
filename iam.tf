4e9f62f471c2c15c8d0474c35a815e9ad6ccbd43
Rhys Emmas <rhys.emmas@ntlworld.com>
2019-01-31 15:05:27 +0000

    added permissions to IAM policy for AWS parameters

--- iam.tf
+++ iam.tf
@@ -42,6 +42,10 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_port",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_admin_username",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_admin_password"
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_provider_client_id",
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_client_secret",
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_provider_config_url",
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_tableau_server_external_url"
       ]
     }
   ]
