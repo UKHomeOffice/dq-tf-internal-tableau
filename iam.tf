8e69587ebdc24173752be1fac5ac4fdcb70ad8b5
Ian Relf <ian.relf@gmail.com>
2019-02-15 17:08:04 +0000

    Adding Tableau automation scripts and protecting "$ENV" vars.

--- iam.tf
+++ iam.tf
@@ -37,11 +37,15 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_linux_ssh_private_key",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_linux_ssh_public_key",
         "arn:aws:ssm:eu-west-2:*:parameter/data_archive_tab_int_backup_url",
-        "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_url",
+        "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_protocol",
+        "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_user",
         "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_host",
         "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_port",
+        "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_org",
+        "arn:aws:ssm:eu-west-2:*:parameter/tab_int_repo_name",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_admin_username",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_admin_password",
+        "arn:aws:ssm:eu-west-2:*:parameter/gp_db_user_wsr",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_provider_client_id",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_client_secret",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_openid_provider_config_url",
@@ -49,7 +53,8 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_product_key_1",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_product_key_2",
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_product_key_3",
-        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_product_key_4"
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_product_key_4",
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_publish_datasources"
       ]
     }
   ]
