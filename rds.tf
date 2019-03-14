a42c9cd6e759d204579667451de6d6828e967f78
Duncan Mountford <duncan9562@gmail.com>
2019-03-14 14:07:39 +0000

    Add Dev and QA instances

--- rds.tf
+++ rds.tf
@@ -98,6 +98,77 @@ resource "aws_db_instance" "postgres" {
   }
 }
 
+resource "aws_db_instance" "internal_reporting_snapshot_dev" {
+  snapshot_identifier                 = "internal-reporting-20190301-1057"
+  auto_minor_version_upgrade          = "true"
+  backup_retention_period             = "14"
+  backup_window                       = "00:00-01:00"
+  copy_tags_to_snapshot               = "false"
+  db_subnet_group_name                = "${aws_db_subnet_group.rds.id}"
+  deletion_protection                 = "false"
+  enabled_cloudwatch_logs_exports     = ["postgresql","upgrade"]
+  iam_database_authentication_enabled = "false"
+  identifier                          = "dev-postgres-${local.naming_suffix}"
+  instance_class                      = "db.t3.large"
+  iops                                = "0"
+  kms_key_id                          = "${aws_kms_key.rds.arn}"
+  license_model                       = "postgresql-license"
+  maintenance_window                  = "mon:01:30-mon:02:30"
+  monitoring_interval                 = "0"
+  multi_az                            = "true"
+  port                                = "5432"
+  publicly_accessible                 = "false"
+  skip_final_snapshot                 = true
+  storage_encrypted                   = true
+  storage_type                        = "gp2"
+  vpc_security_group_ids              = ["${aws_security_group.internal_tableau_db.id}"]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "dev-postgres-${local.naming_suffix}"
+  }
+}
+
+resource "aws_db_instance" "internal_reporting_snapshot_qa" {
+  snapshot_identifier                 = "internal-reporting-20190301-1057"
+  auto_minor_version_upgrade          = "true"
+  backup_retention_period             = "14"
+  backup_window                       = "00:00-01:00"
+  copy_tags_to_snapshot               = "false"
+  db_subnet_group_name                = "${aws_db_subnet_group.rds.id}"
+  deletion_protection                 = "false"
+  enabled_cloudwatch_logs_exports     = ["postgresql","upgrade"]
+  iam_database_authentication_enabled = "false"
+  identifier                          = "qa-postgres-${local.naming_suffix}"
+  instance_class                      = "db.t3.large"
+  iops                                = "0"
+  kms_key_id                          = "${aws_kms_key.rds.arn}"
+  license_model                       = "postgresql-license"
+  maintenance_window                  = "mon:01:30-mon:02:30"
+  monitoring_interval                 = "0"
+  multi_az                            = "true"
+  option_group_name                   = "default:postgres-10"
+  arameter_group_name                 = "default.postgres10"
+  port                                = "5432"
+  publicly_accessible                 = "false"
+  skip_final_snapshot                 = true
+  storage_encrypted                   = true
+  storage_type                        = "gp2"
+  vpc_security_group_ids              = ["${aws_security_group.internal_tableau_db.id}"]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "qa-postgres-${local.naming_suffix}"
+  }
+}
+
+
 resource "aws_ssm_parameter" "rds_internal_tableau_username" {
   name  = "rds_internal_tableau_username"
   type  = "SecureString"
