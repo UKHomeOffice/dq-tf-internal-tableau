f410acbe45594162f56f296fdca1f8ce00cbaeb3
david-wirelab <info@wirelab.co.uk>
2019-03-25 15:49:22 +0000

    move maint window forward

--- rds.tf
+++ rds.tf
@@ -80,7 +80,7 @@ resource "aws_db_instance" "postgres" {
   name                    = "${var.database_name}"
   port                    = "${var.port}"
   backup_window           = "00:00-01:00"
-  maintenance_window      = "tue:01:30-tue:02:30"
+  maintenance_window      = "mon:16:05-mon:17:35"
   backup_retention_period = 14
   storage_encrypted       = true
   multi_az                = true
