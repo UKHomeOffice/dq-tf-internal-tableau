b0d1fd89314a7a83953c8a046d179c8256b5dd47
Duncan Mountford <duncan9562@gmail.com>
2019-04-08 13:25:21 +0100

    Pull S3 bucket name from Apps

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -28,6 +28,7 @@ class TestE2E(unittest.TestCase):
               s3_archive_bucket                 = "bucket-name"
               s3_archive_bucket_key             = "1234567890"
               s3_archive_bucket_name            = "bucket-name"
+              s3_haproxy_config_bucket          = "s3-bucket-name"
               haproxy_private_ip                = "1.2.3.4"
               environment                       = "prod"
             }
