ae911f821a700f7c2e00819623a9a91b69ff3d51
Duncan Mountford <duncan9562@gmail.com>
2019-06-06 15:46:30 +0100

    Change location of count

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ locals {
 }
 
 module "dq-lambda-run-command-ec2" {
-  count_tag          = "${var.environment == "prod" ? "0" : "1"}"                       # Used as 'count', as count is not supported in modules
   source             = "github.com/UKHomeOffice/dq-lambda-run-command-ec2"
+  count_tag          = "${var.environment == "prod" ? "0" : "1"}"                       # Used as 'count', as count is not supported in modules  
   namespace          = "${var.environment}"
   instance_id        = "${aws_instance.int_tableau_linux.id}"
   ip_address         = ""
