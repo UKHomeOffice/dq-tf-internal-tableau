c35c420d67576419965779cdf8c3a540eaed4cdb
Jwanderema <wanderema@hotmail.co.uk>
2019-06-25 10:33:11 +0100

    turn on instance and set AMI to 175

--- main.tf
+++ main.tf
@@ -206,7 +206,7 @@ EOF
 }
 
 resource "aws_instance" "int_tableau_linux_staging" {
-  count                       = "${var.environment == "prod" ? "1" : "0"}"         # Allow different instance count in prod and notprod
+  count                       = "${var.environment == "prod" ? "1" : "1"}"         # Allow different instance count in prod and notprod
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.int_tableau_linux.id}"
   instance_type               = "c5.4xlarge"
