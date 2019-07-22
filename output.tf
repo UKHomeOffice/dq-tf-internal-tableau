da70f5b9c53b6041397b12047f7e76a62eee2a09
Duncan Mountford <duncan9562@gmail.com>
2019-07-22 10:51:45 +0100

    Add to list

--- output.tf
+++ output.tf
@@ -14,5 +14,5 @@ output "rds_internal_tableau_address" {
 }
 
 output "rds_internal_tableau_staging_endpoint" {
-  value = "${var.environment == "prod" ? '["${aws_db_instance.internal_reporting_snapshot_stg.*.endpoint}"]' : "0"}"
+  value = ["${var.environment == "prod" ? "${aws_db_instance.internal_reporting_snapshot_stg.*.endpoint}" : "0"}"]
 }
