a4a49cfac59071ea6b1b78e6a5ace181da863cb6
Jwanderema <wanderema@hotmail.co.uk>
2019-10-09 15:31:14 +0100

    spin up a blue for patching

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ locals {
 #}
 
 resource "aws_instance" "int_tableau_linux" {
-  count                       = "${var.environment == "prod" ? "1" : "1"}"                       # Allow different instance count in prod and notprod
+  count                       = "${var.environment == "prod" ? "2" : "1"}"                       # Allow different instance count in prod and notprod
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.int_tableau_linux.id}"
   instance_type               = "r5d.4xlarge"
