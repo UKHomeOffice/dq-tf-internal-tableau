4a7d0415a3b76822b5480f083e0cfe43b6000570
Jwanderema <wanderema@hotmail.co.uk>
2020-01-16 13:23:05 +0000

    raise count notprod by 1

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ locals {
 #}
 
 resource "aws_instance" "int_tableau_linux" {
-  count                       = "${var.environment == "prod" ? "2" : "1"}"                       # Allow different instance count in prod and notprod
+  count                       = "${var.environment == "prod" ? "2" : "2"}" # Allow different instance count in prod and notprod
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.int_tableau_linux.id}"
   instance_type               = "r5d.4xlarge"
@@ -211,7 +211,7 @@ EOF
 }
 
 resource "aws_instance" "int_tableau_linux_staging" {
-  count                       = "${var.environment == "prod" ? "1" : "0"}"         # Allow different instance count in prod and notprod
+  count                       = "${var.environment == "prod" ? "1" : "0"}" # Allow different instance count in prod and notprod
   key_name                    = "${var.key_name}"
   ami                         = "${data.aws_ami.int_tableau_linux.id}"
   instance_type               = "c5.4xlarge"
