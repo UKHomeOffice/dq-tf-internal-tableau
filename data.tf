3d37e05ac84d993308cac4e23722130df1b14ccb
Ian Relf <ian.relf@gmail.com>
2020-02-27 08:27:16 +0000

    Use upgraded Tableau 2020 for Staging

--- data.tf
+++ data.tf
@@ -14,6 +14,22 @@ data "aws_ami" "int_tableau_linux" {
   ]
 }
 
+data "aws_ami" "int_tableau_linux_upgrade" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "dq-tableau-linux-193*",
+    ]
+  }
+
+  owners = [
+    "self",
+  ]
+}
+
 data "aws_kms_key" "rds_kms_key" {
   key_id = "alias/aws/rds"
 }
