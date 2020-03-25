81b8d4efce4c8d98a1178f296bc8466fb5b35ab2
Ian Relf <ian.relf@gmail.com>
2020-03-25 09:52:26 +0000

    Add permission to IAM Role to be able to pull username/passwords from Parameter Store.

--- iam.tf
+++ iam.tf
@@ -70,7 +70,11 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/tableau_int_publish_workbooks",
         "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_postgres_endpoint",
         "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_dev_endpoint",
-        "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_qa_endpoint"
+        "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_qa_endpoint",
+        "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_service_username",
+        "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_service_password",
+        "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_username",
+        "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_password"
       ]
     },
     {
