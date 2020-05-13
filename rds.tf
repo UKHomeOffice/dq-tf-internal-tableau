599575c29540ccdf388e5d55e291e43fa6362382
Jwanderema <wanderema@hotmail.co.uk>
2020-05-13 11:20:01 +0100

    resize rds

--- rds.tf
+++ rds.tf
@@ -97,7 +97,7 @@ EOF
 
 resource "aws_db_instance" "postgres" {
   identifier                      = "postgres-${local.naming_suffix}"
-  allocated_storage               = "${var.environment == "prod" ? "3300" : "350"}"
+  allocated_storage               = "${var.environment == "prod" ? "3300" : "400"}"
   storage_type                    = "gp2"
   engine                          = "postgres"
   engine_version                  = "${var.environment == "prod" ? "10.10" : "10.10"}"
