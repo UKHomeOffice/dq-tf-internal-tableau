9a1b831f99d59c8814ecd9ab2f706cb2b51c66f7
Jwanderema <wanderema@hotmail.co.uk>
2020-09-24 16:21:02 +0100

    reduce instance size and make single AZ

--- rds.tf
+++ rds.tf
@@ -245,13 +245,13 @@ resource "aws_db_instance" "internal_reporting_snapshot_stg" {
   enabled_cloudwatch_logs_exports     = ["postgresql", "upgrade"]
   iam_database_authentication_enabled = "false"
   identifier                          = "stg-postgres-${local.naming_suffix}"
-  instance_class                      = var.environment == "prod" ? "db.m5.4xlarge" : "db.m5.2xlarge"
+  instance_class                      = var.environment == "prod" ? "db.m5.2xlarge" : "db.m5.2xlarge"
   iops                                = "0"
   kms_key_id                          = data.aws_kms_key.rds_kms_key.arn
   license_model                       = "postgresql-license"
   backup_window                       = var.environment == "prod" ? "00:00-01:00" : "07:00-08:00"
   maintenance_window                  = var.environment == "prod" ? "tue:01:00-tue:02:00" : "mon:08:00-mon:09:00"
-  multi_az                            = var.environment == "prod" ? "true" : "false"
+  multi_az                            = var.environment == "prod" ? "false" : "false"
   port                                = "5432"
   publicly_accessible                 = "false"
   skip_final_snapshot                 = true
