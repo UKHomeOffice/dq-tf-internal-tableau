01057f0a7d060b8e8e3df72829aed62a8feffaa7
Jwanderema <wanderema@hotmail.co.uk>
2020-09-25 10:12:23 +0100

    remove staging rds

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -108,14 +108,14 @@ class TestE2E(unittest.TestCase):
     def test_iam_role(self):
         self.assertEqual(self.runner.get_value("module.root_modules.aws_iam_role.postgres", "name"), "rds-postgres-role-internal-tableau-apps-preprod-dq")
 
-    def test_staging_instance_tag(self):
-        self.assertEqual(self.runner.get_value("module.root_modules.aws_instance.int_tableau_linux_staging[0]", "tags"), {"Name": "ec2-staging-internal-tableau-apps-preprod-dq"})
-
-    def test_rds_staging_tags(self):
-        self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.internal_reporting_snapshot_stg[0]", "tags"), {"Name": "stg-postgres-internal-tableau-apps-preprod-dq"})
-
-    def test_rds_staging_tags(self):
-        self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.internal_reporting_snapshot_stg[0]", "tags"), {"Name": "stg-postgres-internal-tableau-apps-preprod-dq"})
+    # def test_staging_instance_tag(self):
+    #     self.assertEqual(self.runner.get_value("module.root_modules.aws_instance.int_tableau_linux_staging[0]", "tags"), {"Name": "ec2-staging-internal-tableau-apps-preprod-dq"})
+    #
+    # def test_rds_staging_tags(self):
+    #     self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.internal_reporting_snapshot_stg[0]", "tags"), {"Name": "stg-postgres-internal-tableau-apps-preprod-dq"})
+    #
+    # def test_rds_staging_tags(self):
+    #     self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.internal_reporting_snapshot_stg[0]", "tags"), {"Name": "stg-postgres-internal-tableau-apps-preprod-dq"})
 
     def test_rds_postgres_backup_window(self):
         self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.postgres", "backup_window"), "00:00-01:00")
