7a0a58bc8a7a54c5c6eca127446717a32af3ab0f
Jwanderema <wanderema@hotmail.co.uk>
2021-02-16 10:44:22 +0000

    rds_alarm

--- versions.tf
+++ versions.tf
@@ -1,9 +1,8 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    archive = "~> 1.3"
-    aws     = "~> 2.70"
-    random  = "~> 2.3"
+    aws    = "~> 2.70"
+    random = "~> 2.3"
   }
 
 }
