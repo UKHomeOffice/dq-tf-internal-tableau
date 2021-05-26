85c9bef4cc56080f733be3ccb1a39935362dac3a
JWanderema <wanderema@hotmail.co.uk>
2021-05-26 15:43:49 +0100

    add path module

--- variables.tf
+++ variables.tf
@@ -240,6 +240,10 @@ variable "kms_key_s3" {
   }
 }
 
-# locals {
-#   path_module = var.path_module != "unset" ? var.path_module : path.module
-# }
+locals {
+  path_module = path.module
+}
+
+variable "path_module" {
+  default = "./"
+}
