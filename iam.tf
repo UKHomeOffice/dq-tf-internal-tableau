c0d2e6e6d7509e9fe858a956f966f7b6fc92cc3e
JWanderema <wanderema@hotmail.co.uk>
2021-09-21 12:09:05 +0100

    add permissions to newly named smtp param

--- iam.tf
+++ iam.tf
@@ -74,7 +74,7 @@ resource "aws_iam_role_policy" "int_tableau" {
         "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_service_password",
         "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_username",
         "arn:aws:ssm:eu-west-2:*:parameter/rds_internal_tableau_password",
-        "arn:aws:ssm:eu-west-2:*:parameter/tableau_config_smtp"
+        "arn:aws:ssm:eu-west-2:*:parameter/tableau_config_smtp_int"
       ]
     },
     {
