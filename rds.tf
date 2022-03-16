dedf1aa1edf930479d7554c9a194d5c1634dd6c2
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-03-16 14:22:11 +0000

    increase storage

--- rds.tf
+++ rds.tf
@@ -103,7 +103,7 @@ resource "aws_iam_role_policy_attachment" "dq_tf_infra_write_to_cw_rds" {
 
 resource "aws_db_instance" "postgres" {
   identifier                      = "postgres-${local.naming_suffix}"
-  allocated_storage               = var.environment == "prod" ? "3630" : "1000"
+  allocated_storage               = var.environment == "prod" ? "3630" : "1500"
   storage_type                    = "gp2"
   engine                          = "postgres"
   engine_version                  = var.environment == "prod" ? "10.13" : "10.13"
