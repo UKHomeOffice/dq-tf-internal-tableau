2bae800a694a55ca57bb930b0c923badf3aa70d8
JWanderema <wanderema@hotmail.co.uk>
2022-05-04 15:56:40 +0100

    add test

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -140,7 +140,7 @@ class TestE2E(unittest.TestCase):
         self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.postgres", "engine_version"), "10.18")
 
     def test_rds_postgres_postgres_parameter_grop(self):
-        self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.postgres", "engine_version"), "DQCustom")
+        self.assertEqual(self.runner.get_value("module.root_modules.aws_db_instance.postgres", "engine_version"), "DQ.postgres10")
 
 
 if __name__ == '__main__':
