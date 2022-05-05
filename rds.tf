3b3eaaf76b82890ba70e89e8d292c0c38e0b9eda
JWanderema <wanderema@hotmail.co.uk>
2022-05-05 13:02:19 +0100

    correct postgres family

--- rds.tf
+++ rds.tf
@@ -352,7 +352,7 @@ resource "aws_ssm_parameter" "rds_internal_tableau_stg_endpoint" {
 
 resource "aws_db_parameter_group" "DQCustom" {
   name   = "dqpostgresx"
-  family = "postgres1"
+  family = "postgres10"
 
   parameter {
     name  = "work_mem"
