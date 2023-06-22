38557472d561a5057b70b67a73af994cc6f50f99
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-06-22 10:55:01 +0100

    TEMPORARILY hardcoding to AMI #533 in CI account

--- data.tf
+++ data.tf
@@ -6,16 +6,28 @@ data "aws_ami" "int_tableau_linux" {
 
     # "dq-tableau-linux-nnn" is used to pull exact image
     # "copied from*" is used to pull copy of nnn image copied to NotProd/Prod
+    #values = [
+    #  "dq-tableau-linux-667 copied from*"
+    #]
+
+    # TEMPORARILY hardcoding to 533 (the version used in Prod)
     values = [
-      "dq-tableau-linux-667 copied from*"
+      "dq-tableau-linux-533"
     ]
+
   }
 
   # "self" is used to ensure that NotProd uses image copied to NotProd account
   # and Prod uses image copied to Prod account
+  #owners = [
+  #  "self",
+  #]
+
+  # TEMPORARILY using the CI account - to use the CI AMI in NotProd & Prod
   owners = [
-    "self",
+    "093401982388",
   ]
+
 }
 
 data "aws_kms_key" "rds_kms_key" {
