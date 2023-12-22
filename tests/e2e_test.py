92e124e5769bb789fff211ec6e0492e4b1ebceae
Ian Relf <ian.relf@digital.homeoffice.gov.uk>
2023-12-22 18:37:38 +0000

    Migrate TF 0.12 -> 0.13  - inc versions.tf  - inc tests

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -12,7 +12,6 @@ class TestE2E(unittest.TestCase):
               region = "eu-west-2"
               profile = "foo"
               skip_credentials_validation = true
-              skip_get_ec2_platforms = true
             }
             module "root_modules" {
               source = "./mymodule"
