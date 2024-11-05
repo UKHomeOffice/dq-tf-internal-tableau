6608badbf25546c05539ea175076bd8edb9699c4
Gurukishore Bolisetty <gurukishore.bolisetty1@digital.homeoffice.gov.uk>
2024-11-05 15:33:08 +0000

    updated aws & archive versions

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.4.0"
+      version = "2.4.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.12.0"
+      version = "~> 5.31.0"
     }
     random = {
       source  = "hashicorp/random"
