e7aa1d36f898949300f524d05dec7cacc020ecc5
D-Gurukishore Bolisetty <d-boliseg@U-16XTRFGVJW9XA.dev.dacc.phz>
2026-03-19 12:24:03 +0000

    Python runtime upgrade

--- alarms_lambda.tf
+++ alarms_lambda.tf
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "lambda_slack" {
   role             = aws_iam_role.lambda_role_slack.arn
   handler          = "slack.lambda_handler"
   source_code_hash = data.archive_file.lambda_slack_zip.output_base64sha256
-  runtime          = "python3.7"
+  runtime          = "python3.11"
   timeout          = "60"
 
   tags = {
